4575b15163ae7ff100457e05a79dde1cb90e8b94
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-05-20 23:07:45 +0100

    added platform extensions as separate class

---
+++ RetroTransferLibrary/PlatformExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetroTransferLibrary
+{
+    public class PlatformExtensions
+    {
+        private Dictionary<List<string>, string> platformExtensions = new Dictionary<List<string>, string>();
+        
+        /// <summary>
+        /// Constructor that establishes the key value pairs.
+        /// </summary>
+        public PlatformExtensions()
+        {
+            initPlatformExtensions();
+        }
+
+        /// <summary>
+        /// Takes a string containing the current file extension and returns the name of the platform to which the file belongs.
+        /// Returns null if the platform is unknown.
+        /// </summary>
+        /// <param name="extension"></param>
+        /// <returns></returns>
+        public string GetPlatform(string extension)
+        {
+            foreach (List<string> currentPlatformExtensions in platformExtensions.Keys)
+            {
+                if (currentPlatformExtensions.Contains(extension))
+                {
+                    return platformExtensions[currentPlatformExtensions];
+                }
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Sets up dictionary of key value pairs, where the keys are a list of possible extensions and the value is the platform.
+        /// </summary>
+        private void initPlatformExtensions()
+        {
+            platformExtensions[new List<string> { "gba" }] = "gba";
+            platformExtensions[new List<string> { "gb" }] = "gb";
+            platformExtensions[new List<string> { "gbc" }] = "gbc";
+            platformExtensions[new List<string> { "z64", "n64", "v64" }] = "n64";
+            platformExtensions[new List<string> { "nes" }] = "nes";
+            platformExtensions[new List<string> { "smc", "sfc" }] = "snes";
+        }
+    }
+}
---
+++ RetroTransferLibrary/Rom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetroTransferLibrary
+{
+    public class Rom
+    {
+        public string FileName { get; set; }
+        public string LocalPath { get; set; }
+        public string Platform { get; set; }
+        public string PlatformSubPath { get; set; }
+        public Rom (string localPath)
+        {
+            LocalPath = localPath;
+        }
+    }
+}
